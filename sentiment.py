db6064d6cc3d858a4754c58d19eb0b35590f9d7e
Mainak Biswas <damaniac.vic1251998@gmail.com>
2018-03-19 16:49:19 +0530

    Add files via upload
    Added Sklearn's Logistic Regression classifier for testing.

--- sentiment.py
+++ sentiment.py
@@ -41,8 +41,9 @@ train = pd.DataFrame({"target":train[0],"id":train[1],"date":train[2],"flag":tra
 
 from sklearn.utils import shuffle
 train = shuffle(train)
-train = train.sample(frac=1).reset_index(drop=True)
-train = train[:10000]
+train = train.sample(frac=0.2).reset_index(drop=True)
+train = train[:50]
+test = train[50:100]
 # print(train.shape)
 
 
@@ -70,7 +71,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 # print(stopwords.words("english"))
 lmtzr = WordNetLemmatizer()
 stemmer = PorterStemmer()
-cv = CountVectorizer(analyzer="word",stop_words=stopwords.words("english"),max_features=10000)
+cv = CountVectorizer(analyzer="word",stop_words=stopwords.words("english"),max_features=5000)
 
 def cleaning(text) :
     text = BeautifulSoup(text).get_text()
@@ -88,7 +89,6 @@ for i in range(0,train["text"].size) :
 
 train_feats = cv.fit_transform(training_clean)
 train_feats = train_feats.toarray()
-# print(train_feats)
 # print(train_feats.shape)
 # print(training_clean)
 # print(train["text"][0])
@@ -104,12 +104,58 @@ train_feats = train_feats.toarray()
 # MODELLING
 
 target = train["target"]
+# print(target)
 train.drop("target",axis=1,inplace=True)
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression()
-param = {"C":[0.1]}
-clf = GridSearchCV(lr,param_grid=param,cv=10,n_jobs=-1)
-clf.fit(train_feats,target)
-print(clf.best_score_)
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.linear_model import LogisticRegression
+# lr = LogisticRegression()
+# param = {"C":[0.1]}
+# clf = GridSearchCV(lr,param_grid=param,cv=10,n_jobs=-1)
+# clf.fit(train_feats,target)
+# print(clf.best_score_)
+
+
+# IMPLEMENTATION OF LOGISTIC REGRESSION
+
+def sigmoid(inp):
+    return (1 / (1 + np.exp(-inp)))
+
+def loglike(features,target,weights) :
+    score = np.dot(features,weights)
+    ll = np.sum(target*score - np.log(1+np.exp(score)))
+    return ll
+
+def logistic(features,target,steps,learning_rate,intercept=False) :
+    if intercept :
+        intercept = np.ones((features.shape[0],1))
+        features = np.hstack((intercept,features))
+
+    weights = np.zeros(features.shape[1])
+    for step in range(0,steps) :
+        score = np.dot(features,weights)
+        prediction = sigmoid(score)
+        error = target - prediction
+        grad = np.dot(features.T,error)
+        weights = weights + learning_rate*grad
+
+    return weights
+
+weights = logistic(train_feats,target,steps=300000,learning_rate=0.01,intercept=True)
+#
+# print(weights)
+# print(len(weights))
+
+# GETTING LABELS BASED ON WEIGHTS
+
+# print(train_feats.shape[0])
+data = np.hstack((np.ones((train_feats.shape[0],1)),train_feats))
+print(data.shape)
+final = np.dot(data,weights)
+pred = np.round(sigmoid(final))
+
+print(pred)
+print(target)
+# print(len(pred))
+
+# CHECKING ACCURACY
+# print((pred==target).sum().astype(float)/len(pred))
